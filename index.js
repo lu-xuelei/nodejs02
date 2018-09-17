520e837606c3e32bf532cd4129e916a1f969f29d
Xuelei <Lu Xuelei>
2018-09-17 08:44:42 +0800

    Add logic to read env value, to handle GET request and working with request params

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ let express = require("express");
 
 const app = express();
 
+const courses = [
+  {
+    id: 1,
+    name: "Course 1"
+  },
+  {
+    id: 1,
+    name: "Course 1"
+  },
+  {
+    id: 2,
+    name: "Course 2"
+  },
+  {
+    id: 3,
+    name: "Course 3"
+  }
+];
 /**
  * app.get will listen to get requests
  */
@@ -9,8 +27,32 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello Express");
 });
 
-app.get("/api/numbers", (req, res) => {
-  res.status(200).send([1, 2, 3, 4, 5]);
+/**
+ * Return the list of courses
+ */
+app.get("/api/courses", (req, res) => {
+  res.status(200).send(courses);
 });
 
-app.listen(3000, () => console.log("Listening to port 3000"));
+/**
+ * Return course with the givne ID if found
+ */
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("Course with the given id is not found.");
+  }
+  res.send(course);
+});
+
+/**
+ * Try to log port from environment if available
+ * To set a env variable:
+ * Unix: export PORT=5000
+ * CMD: set PORT=5000
+ * PS: $env:PORT=5000
+ */
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
